f74d3894eabc0c1174dee6a832ee1d4beddfda45
Necip Dirik <necipdirik@gmail.com>
2024-03-27 12:10:26 +0300

    validating user input added

--- scripts/config.js
+++ scripts/config.js
@@ -6,12 +6,21 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
   backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
   console.dir(event);
   const formData = new FormData(event.target);
-  const enteredPlayername = formData.get('playername');
+  const enteredPlayername = formData.get('playername').trim(); // '      ' => ''
   console.log(enteredPlayername);
+
+  if (!enteredPlayername) {
+    // enteredPlayername === ''
+    event.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please enter a valid name!';
+    return;
+  }
 }
